5f472077e0cdff6f3cd0da58f61e9f098cc7a756
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2019-09-22 15:33:09 -0400

    update code for multi-level models

--- brm.R
+++ brm.R
@@ -1,2 +1,2 @@
 library(brms)
-m9a <- brm(Mentions ~ is_sync + (1|screen_name), family = "poisson", data = prepped_data) # warning
+m9a <- brm(Mentions ~ is_sync + (1|screen_name), family = "poisson", data = prepped_data, chains = 4, cores = 4) # warning
